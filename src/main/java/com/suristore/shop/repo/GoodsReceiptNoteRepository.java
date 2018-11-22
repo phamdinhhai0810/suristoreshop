c61d5f5807f58fa2c730afc4a475700be1dc82af
phamdinhhai0810 <phamdinhhai0810@gmail.com>
2018-11-22 16:59:50 +0700

    Update GoodsReceiptNoteRepository.java

--- src/main/java/com/suristore/shop/repo/GoodsReceiptNoteRepository.java
+++ src/main/java/com/suristore/shop/repo/GoodsReceiptNoteRepository.java
@@ -6,4 +6,19 @@ import com.suristore.shop.domain.GoodsReceiptNote;
 
 public interface GoodsReceiptNoteRepository extends JpaRepository<GoodsReceiptNote, Integer> {
 
+  @Query(nativeQuery = true, value = " SELECT created_at as datadate, SUM(price) as totalPrice "
+			 						 + " FROM goods_receipt_note "
+									 + " GROUP BY YEAR(created_at), MONTH(created_at) "
+									 + " ORDER BY YEAR(created_at) DESC, MONTH(created_at) DESC "
+									 + " LIMIT 12 ")
+	List<StatisticTotalPriceInterface> statisticMonthTotalPrice();
+
+	@Query(nativeQuery = true,value =  " SELECT created_at as datadate, SUM(price) as totalPrice "
+									+ " FROM goods_receipt_note "
+									+ " WHERE MONTH(created_at) = MONTH(CURRENT_DATE()) "
+									+ " AND YEAR(created_at) = YEAR(CURRENT_DATE()) "
+									+ " GROUP BY  DAY(created_at) "
+									+ " ORDER BY  DAY(created_at) DESC")
+	List<StatisticTotalPriceInterface> statisticDayTotalPrice();
+  
 }
