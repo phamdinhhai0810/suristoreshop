6f34231803cfa86f0198552f25844db2b4a1f458
phamdinhhai0810 <phamdinhhai0810@gmail.com>
2018-11-22 16:55:49 +0700

    Update AdminStatisticController.java

--- src/main/java/com/suristore/shop/controller/AdminStatisticController.java
+++ src/main/java/com/suristore/shop/controller/AdminStatisticController.java
@@ -1,30 +1,36 @@
 package com.suristore.shop.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.suristore.shop.domain.OrderStatisticMonthTotalPriceClass;
-import com.suristore.shop.service.OrderService;
+import com.suristore.shop.service.StatisticService;
 
 @Controller
 @RequestMapping(value = "/admin/statistic")
 public class AdminStatisticController {
 
 	@Autowired
-	private OrderService orderService;
+	private StatisticService statisticService;
 
-	@GetMapping(value = "")
+	@GetMapping(value = { "", "/day" })
 	public String index(Model model) {
 
-		List<OrderStatisticMonthTotalPriceClass> listTotalPrice = orderService.getOrderStatisticMonthTotalPrice();
-		model.addAttribute("totalprice", listTotalPrice);
+		model.addAttribute("totalprice_day", statisticService.getStatisticDayTotalPrice());
+		model.addAttribute("profit_day", statisticService.getStatisticDayProfit());
+		
+		return "admin/statistic_day";
+	}
 
-		return "admin/statistic";
+	@GetMapping(value = { "", "/month" })
+	public String month(Model model) {
+
+		model.addAttribute("totalprice_month", statisticService.getStatisticMonthTotalPrice());
+		model.addAttribute("profit_month", statisticService.getStatisticMonthProfit());
+		
+		return "admin/statistic_month";
 	}
 
 }
