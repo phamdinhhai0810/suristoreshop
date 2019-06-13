6b03cb46ecb77cc84d9b5be00851c59e0516e0f3
phamdinhhai0810 <phamdinhhai0810@gmail.com>
2019-06-13 16:55:20 +0700

    Update TypeServiceImpl.java

--- src/main/java/com/suristore/shop/service/impl/TypeServiceImpl.java
+++ src/main/java/com/suristore/shop/service/impl/TypeServiceImpl.java
@@ -1,46 +1,46 @@
 package com.suristore.shop.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.suristore.shop.domain.Type;
 import com.suristore.shop.repo.TypeRepository;
 import com.suristore.shop.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TypeServiceImpl implements TypeService {
 
-	@Autowired
-	private TypeRepository typeRepository;
+    @Autowired
+    private TypeRepository typeRepository;
 
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Type> findAll() {
-		return typeRepository.findAll();
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public Iterable<Type> findAll() {
+        return typeRepository.findAll(new Sort(Sort.Direction.DESC, "createdAt"));
+    }
 
-	@Override
-	@Transactional(readOnly = true)
-	public Type findOne(int id) {
-		return typeRepository.findOne(id);
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public Type findOne(int id) {
+        return typeRepository.findOne(id);
+    }
 
-	@Override
-	@Transactional(readOnly = true)
-	public long countAll() {
-		return typeRepository.count();
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public long countAll() {
+        return typeRepository.count();
+    }
 
-	@Override
-	@Transactional
-	public Type save(Type type) {
-		return typeRepository.save(type);
-	}
+    @Override
+    @Transactional
+    public Type save(Type type) {
+        return typeRepository.save(type);
+    }
 
-	@Override
-	@Transactional
-	public void delete(int id) {
-		typeRepository.delete(id);
-	}
+    @Override
+    @Transactional
+    public void delete(int id) {
+        typeRepository.delete(id);
+    }
 }
