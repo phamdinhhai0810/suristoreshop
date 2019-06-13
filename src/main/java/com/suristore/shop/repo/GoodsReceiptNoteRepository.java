5c8003d0abd43124bae385061281b4c488962bfd
phamdinhhai0810 <phamdinhhai0810@gmail.com>
2019-06-13 16:56:57 +0700

    Update GoodsReceiptNoteRepository.java

--- src/main/java/com/suristore/shop/repo/GoodsReceiptNoteRepository.java
+++ src/main/java/com/suristore/shop/repo/GoodsReceiptNoteRepository.java
@@ -10,19 +10,17 @@ import com.suristore.shop.domain.custom.StatisticTotalPriceInterface;
 
 public interface GoodsReceiptNoteRepository extends JpaRepository<GoodsReceiptNote, Integer> {
 
-	@Query(nativeQuery = true, value = " SELECT created_at as datadate, SUM(price) as totalPrice "
-			 						 + " FROM goods_receipt_note "
-									 + " GROUP BY YEAR(created_at), MONTH(created_at) "
-									 + " ORDER BY YEAR(created_at) DESC, MONTH(created_at) DESC "
-									 + " LIMIT 12 ")
-	List<StatisticTotalPriceInterface> statisticMonthTotalPrice();
+    @Query(nativeQuery = true, value = " SELECT DATE_FORMAT(created_at ,'%Y-%m') as datadate, SUM(price) as totalPrice "
+            + " FROM goods_receipt_note "
+            + " WHERE YEAR(created_at) = YEAR(current_date()) "
+            + " GROUP BY YEAR(created_at), MONTH(created_at) ")
+    List<StatisticTotalPriceInterface> statisticMonthTotalPrice();
 
-	@Query(nativeQuery = true,value =  " SELECT created_at as datadate, SUM(price) as totalPrice "
-									+ " FROM goods_receipt_note "
-									+ " WHERE MONTH(created_at) = MONTH(CURRENT_DATE()) "
-									+ " AND YEAR(created_at) = YEAR(CURRENT_DATE()) "
-									+ " GROUP BY  DAY(created_at) "
-									+ " ORDER BY  DAY(created_at) DESC")
-	List<StatisticTotalPriceInterface> statisticDayTotalPrice();
-	
+    @Query(nativeQuery = true, value = " SELECT DATE_FORMAT(created_at ,'%Y-%m-%d') as datadate, SUM(price) as totalPrice "
+            + " FROM goods_receipt_note "
+            + " WHERE MONTH(created_at) = MONTH(CURRENT_DATE()) "
+            + " AND YEAR(created_at) = YEAR(CURRENT_DATE()) "
+            + " GROUP BY  DAY(created_at) ")
+    List<StatisticTotalPriceInterface> statisticDayTotalPrice();
+
 }
